5cf3ace504ddcac9a86aa60fd50bb9729888de30
renevoorburg <voorburg@xs4all.nl>
2026-01-03 19:44:34 +0100

    moving around

--- src/main.asm
+++ src/main.asm
@@ -10,39 +10,97 @@ jmp main
 .const SCREEN_WIDTH  = 40
 .const SCREEN_HEIGHT = 25
 
-.const ROW = $fd
-.const COL = $fe
+.const ROW = $fb
+.const COL = $fc
+
+.const  SCRPTRL = $fd
+.const  SCRPTRH = $fe
+
+.const CURCHAR = $ff
+
+.const  SCREEN = $0400
+
+.const CRSRCHR = 160
+.const SPACECHR = 32
 
 main:
-    lda #$93
-    jsr $ffd2
+    // blank screen
+    // lda #$93
+    // jsr $ffd2
 
-    lda #0
+    // prepare keyboard
+    lda #1
+    sta $0289  //  disable keyboard buffer
+    lda #127
+    sta $028a  // disable key repeat
+
+    // initial position
+    lda #5
+    sta ROW
     sta COL
 
-new_column:
-    lda #0
-    sta ROW
-
-column_print:
+print:
     ldx ROW
     ldy COL
+    jsr get_char
+    sta CURCHAR
+    lda #CRSRCHR
     jsr plot_char
 
-    inc ROW
-    lda ROW
-    cmp #SCREEN_HEIGHT
-    bne column_print
+readkey:   
+    jsr $ff9f
+    jsr $ffe4
+    beq readkey
+    cmp #$41         // 'A' in PETSCII
+    beq left
+    cmp #$53         // 'S' in PETSCII
+    beq right
+    cmp #$57         // 'W' in PETSCII
+    beq up
+    cmp #$5A         // 'Z' in PETSCII
+    beq down
+    cmp #$51         // 'Q' in PETSCII
+    beq end
 
-    inc COL
+    jmp readkey
+    
+left:
     lda COL
-    cmp #SCREEN_WIDTH
-    bne new_column
+    beq readkey
+    lda CURCHAR
+    jsr plot_char
+    dec COL
+    jmp print
+
+right: 
+    lda COL
+    cmp #(SCREEN_WIDTH-1)
+    beq readkey
+    lda CURCHAR
+    jsr plot_char
+    inc COL
+    jmp print   
+
+up: 
+    lda ROW
+    beq readkey
+    lda CURCHAR
+    jsr plot_char
+    dec ROW
+    jmp print
+
+down: 
+    lda ROW
+    cmp #(SCREEN_HEIGHT-1)
+    beq readkey
+    lda CURCHAR
+    jsr plot_char
+    inc ROW
+    jmp print
+
+end:
     rts
 
-.const  SCRPTRL = $fb
-.const  SCRPTRH = $fc
-.const  SCREEN = $0400
 
 rowlo:
     .fill 25, <(SCREEN + i*40)
@@ -50,12 +108,23 @@ rowhi:
     .fill 25, >(SCREEN + i*40)
 
 plot_char:
+    pha
     ldx ROW
     lda rowlo,x
     sta SCRPTRL
     lda rowhi,x
     sta SCRPTRH
     ldy COL
-    lda #'1'
+    pla
     sta (SCRPTRL),y
+    rts
+
+get_char:
+    ldx ROW
+    lda rowlo,x
+    sta SCRPTRL
+    lda rowhi,x
+    sta SCRPTRH
+    ldy COL
+    lda (SCRPTRL),y
     rts
